19384feea0073d9fae3e74f6b96d5f653f194899
Odile078 <ouwimpuhwe620@daviscollege.com>
2022-11-02 14:52:56 +0200

    Updating Hero component design

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,11 @@ module.exports = {
       colors:{
         brandBlack:{
           DEFAULT:"RGB(34,34,34)",
-          dark:"#1C1C1C"
-          
+          dark:"#1C1C1C",
+          light:"#CFCFCF"       
+        },
+        brandWhite:{
+          DEFAULT:'#F6F6F6'
         }
       },
       spacing:{
@@ -21,13 +24,19 @@ module.exports = {
       },
       fontSize:{
         '80':'5rem',//80px
+        '40':'2.5rem',//40px
         '46':'2.875rem',//46px
       },
       lineHeight:{
         '30':'1.875rem',//30px
+        '58':'3.625rem',//58px
       },
       backgroundImage:{
         heroBackground:"url('/images/hero-background.png')"
+      },
+      fontFamily:{
+        mont:"'Mont','sans-serif'",
+        montHeavy:"'MontHeavy','sans-serif'",
       }
     },
   },
